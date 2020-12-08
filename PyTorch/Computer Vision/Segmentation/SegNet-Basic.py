3c321c63722cfb32d1490b3bd4634591d6fcd73f
Georgy Trush <48649707+geotrush@users.noreply.github.com>
2020-12-09 00:21:04 +0300

    Update SegNet-Basic.py

--- PyTorch/Computer Vision/Segmentation/SegNet-Basic.py
+++ PyTorch/Computer Vision/Segmentation/SegNet-Basic.py
@@ -20,18 +20,15 @@ class SegNet_Basic(Module):
         self.enc_conv4 = Sequential(Conv2d(256, 512, kernel_size=3, padding=1), BatchNorm2d(512), ReLU())
         self.pool4 = MaxPool2d(kernel_size=2, stride=2, return_indices=True)
 
-        # Bottleneck
-        self.bottleneck = Conv2d(512, 512, kernel_size=3, padding=1)
-
         # Decoder
-        self.unpool1 = MaxUnpool2d(kernel_size=2, stride=2)
-        self.dec_conv1 = Sequential(ConvTranspose2d(512, 256, kernel_size=3, padding=1), BatchNorm2d(256), ReLU())
-        self.unpool2 = MaxUnpool2d(kernel_size=2, stride=2)
-        self.dec_conv2 = Sequential(ConvTranspose2d(256, 128, kernel_size=3, padding=1), BatchNorm2d(128), ReLU())
-        self.unpool3 = MaxUnpool2d(kernel_size=2, stride=2)
-        self.dec_conv3 = Sequential(ConvTranspose2d(128, 64, kernel_size=3, padding=1), BatchNorm2d(64), ReLU())
         self.unpool4 = MaxUnpool2d(kernel_size=2, stride=2)
-        self.dec_conv4 = Sequential(ConvTranspose2d(64, num_classes, kernel_size=3, padding=1), Softmax())
+        self.dec_conv4 = Sequential(ConvTranspose2d(512, 256, kernel_size=3, padding=1), BatchNorm2d(256), ReLU())
+        self.unpool3 = MaxUnpool2d(kernel_size=2, stride=2)
+        self.dec_conv3 = Sequential(ConvTranspose2d(256, 128, kernel_size=3, padding=1), BatchNorm2d(128), ReLU())
+        self.unpool2 = MaxUnpool2d(kernel_size=2, stride=2)
+        self.dec_conv2 = Sequential(ConvTranspose2d(128, 64, kernel_size=3, padding=1), BatchNorm2d(64), ReLU())
+        self.unpool1 = MaxUnpool2d(kernel_size=2, stride=2)
+        self.dec_conv1 = Sequential(ConvTranspose2d(64, num_classes, kernel_size=3, padding=1), Softmax())
         
         # Weight Initialization
         self._initialize_weights(self.enc_conv0, self.enc_conv1, self.enc_conv2, self.enc_conv3,
@@ -55,12 +52,9 @@ class SegNet_Basic(Module):
         e3, e3_idx = self.pool3(self.enc_conv3(e2))
         e4, e4_idx = self.pool4(self.enc_conv4(e3))
 
-        # Bottleneck
-        b = self.bottleneck(e4)
-
         # Decoder
-        d1 = self.dec_conv1(self.unpool1(b,  e4_idx))
-        d2 = self.dec_conv2(self.unpool2(d1, e3_idx))
-        d3 = self.dec_conv3(self.unpool3(d2, e2_idx))
-        d4 = self.dec_conv4(self.unpool4(d3, e1_idx))
-        return d4
+        d4 = self.dec_conv1(self.unpool1(e4, e4_idx))
+        d3 = self.dec_conv2(self.unpool2(d4, e3_idx))
+        d2 = self.dec_conv3(self.unpool3(d3, e2_idx))
+        d1 = self.dec_conv4(self.unpool4(d2, e1_idx))
+        return d1
