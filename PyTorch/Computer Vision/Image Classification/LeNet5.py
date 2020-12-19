639308b71407c7726725b42c1652372f287c1bae
Georgy Trush <48649707+geotrush@users.noreply.github.com>
2020-12-19 23:43:30 +0300

    Create LeNet5.py

---
+++ PyTorch/Computer Vision/Image Classification/LeNet5.py
@@ -0,0 +1,30 @@
+'''
+"Gradient-Based Learning Applied to Document Recognition" (LeCun et al., 1989):
+https://pdfs.semanticscholar.org/62d7/9ced441a6c78dfd161fb472c5769791192f6.pdf
+'''
+from torch.nn import Module, Sequential, Conv2d, ReLU, MaxPool2d, Flatten, Linear
+
+
+class LeNet5(Module):
+    def __init__(self, in_channels=1, num_classes=10):
+        super().__init__()
+        
+        # Feature Extractor
+        self.extractor = Sequential(
+            Conv2d(in_channels, 6, kernel_size=5), ReLU(),
+            MaxPool2d(2),
+            Conv2d(6, 16, kernel_size=5), ReLU(),
+            MaxPool2d(2), Flatten()
+        )
+        
+        # Classifier
+        self.fc1 = Sequential(
+            Linear(256, 120), ReLU(),
+            Linear(120, 84), ReLU(),
+            Linear(84, num_classes), ReLU()
+        )
+        
+    def forward(self, x):
+        ext = self.extractor(x)
+        cls = self.cls(ext)
+        return cls
