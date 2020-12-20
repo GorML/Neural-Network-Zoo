18954fe5143ff26426fe01e673c828898dc00621
Georgy Trush <48649707+geotrush@users.noreply.github.com>
2020-12-20 21:50:29 +0300

    Update ResNet.py

--- PyTorch/Computer Vision/Image Classification/ResNet.py
+++ PyTorch/Computer Vision/Image Classification/ResNet.py
@@ -2,7 +2,7 @@
 "Deep Residual Learning for Image Recognition" (He et al., 2015):
 https://arxiv.org/pdf/1512.03385.pdf
 '''
-from torch.nn import Module, Sequential, Conv2d, BatchNorm2d, GroupNorm2d, ReLU, MaxPool2d, AdaptiveAvgPool2d, Dropout, Flatten, Linear, Softmax
+from torch.nn import Module, Sequential, Conv2d, BatchNorm2d, GroupNorm, ReLU, MaxPool2d, AdaptiveAvgPool2d, Flatten, Linear, Softmax
 from torch.nn.init import kaiming_normal_, constant_
 
 
@@ -15,15 +15,18 @@ def Conv_Block(in_channels, out_channels, **kwargs):
 
 
 class Bottleneck(Module):
-    def __init__(self, in_channels, conv_channels, stride, groups, base_width=64, expansion=4):
+    
+    expansion = 4
+    
+    def __init__(self, in_channels, conv_channels, stride=1, downsample=None, groups=1, base_width=64):
         super().__init__()
         
         width = int(in_channels * (base_width / 64)) * groups
         # NVIDIA's ResNet V1.5: stride for downsampling is at the first 1x1 convolution instead of the 3x3 convolution.
         self.conv1   = Conv_Block(in_channels, width, kernel_size=1)
         self.conv2   = Conv_Block(width, width, kernel_size=3, stride, padding=1, dilation=1, groups=groups)
-        self.conv3   = Conv2d(width, conv_channels * expansion, kernel_size=1)
-        self.last_bn = BatchNorm2d(conv_channels * expansion)
+        self.conv3   = Conv2d(width, conv_channels * self.expansion, kernel_size=1)
+        self.last_bn = BatchNorm2d(conv_channels * self.expansion)
 
     def forward(self, x):
         # Identity Shortcut
@@ -37,23 +40,21 @@ class Bottleneck(Module):
 
 
 class ResNet(Module):
-    def __init__(self, block=Bottleneck, layers, num_classes=1000, groups=1, width_per_group=64):
+    def __init__(self, layers, num_classes=1000):
         super().__init__()
         
         self.in_channels = 64
-        self.groups = groups
-        self.base_width = width_per_group
         
         self.conv    = Conv_Block(3, self.conv_channels, kernel_size=7, stride=2, padding=3)
         self.maxpool = MaxPool2d(kernel_size=3, stride=2, padding=1)
         
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.layer1 = self._make_layer(Bottleneck, 64, layers[0])
+        self.layer2 = self._make_layer(Bottleneck, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(Bottleneck, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(Bottleneck, 512, layers[3], stride=2)
         self.avgpool = Sequential(AdaptiveAvgPool2d((1, 1)), Flatten())
         
-        self.fc = Linear(512 * block.expansion, num_classes)
+        self.fc = Sequential(Linear(512 * Bottleneck.expansion, num_classes), Softmax())
 
         """
         Zero-initialize the last BatchNorm2d in each residual branch,
@@ -71,18 +72,18 @@ class ResNet(Module):
             elif isinstance(module, Bottleneck):
                 constant_(module.last_bn.weight, 0)
 
-    def _make_layer(self, block=Bottleneck, conv_channels, blocks, stride=1):
-        if stride != 1 or self.in_channels != conv_channels * block.expansion:
+    def _make_layer(self, conv_channels, blocks, stride=1):
+        if stride != 1 or self.in_channels != conv_channels * Bottleneck.expansion:
             downsample = Sequential(
-                Conv2d(self.in_channels, conv_channels * block.expansion, kernel_size=1, stride),
-                BatchNorm2d(channels * block.expansion)
+                Conv2d(self.in_channels, conv_channels * Bottleneck.expansion, kernel_size=1, stride),
+                BatchNorm2d(conv_channels * Bottleneck.expansion)
             )
 
         layers = []
-        layers.append(block(self.in_channels, conv_channels, stride, downsample, self.groups, self.base_width))
+        layers.append(Bottleneck(self.in_channels, conv_channels, stride, downsample))
         self.in_channels = conv_channels * block.expansion
         for _ in range(1, blocks):
-            layers.append(block(self.in_channels, conv_channels, groups=self.groups, base_width=self.base_width))
+            layers.append(Bottleneck(self.in_channels, conv_channels))
         return Sequential(*layers)
 
     def forward(self, x):
