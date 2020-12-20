662f3e44f5b0a977ac7aab23fb418273313e448d
Georgy Trush <48649707+geotrush@users.noreply.github.com>
2020-12-20 17:53:58 +0300

    Update LeNet-5.py

--- PyTorch/Computer Vision/Image Classification/LeNet-5.py
+++ PyTorch/Computer Vision/Image Classification/LeNet-5.py
@@ -2,7 +2,7 @@
 "Gradient-Based Learning Applied to Document Recognition" (LeCun et al., 1989):
 https://pdfs.semanticscholar.org/62d7/9ced441a6c78dfd161fb472c5769791192f6.pdf
 '''
-from torch.nn import Module, Sequential, Conv2d, ReLU, MaxPool2d, Flatten, Linear
+from torch.nn import Module, Sequential, Conv2d, Tanh, MaxPool2d, Flatten, Linear
 
 
 class LeNet5(Module):
@@ -11,17 +11,17 @@ class LeNet5(Module):
         
         # Feature Extractor
         self.extractor = Sequential(
-            Conv2d(in_channels, 6, kernel_size=5), ReLU(),
+            Conv2d(in_channels, 6, kernel_size=5), Tanh(),
             MaxPool2d(2),
-            Conv2d(6, 16, kernel_size=5), ReLU(),
+            Conv2d(6, 16, kernel_size=5), Tanh(),
             MaxPool2d(2), Flatten()
         )
         
         # Classifier
         self.classifier = Sequential(
-            Linear(256, 120), ReLU(),
-            Linear(120, 84), ReLU(),
-            Linear(84, num_classes), ReLU()
+            Linear(256, 120), Tanh(),
+            Linear(120, 84), Tanh(),
+            Linear(84, num_classes), Tanh()
         )
         
     def forward(self, x):
