353538b5e89f11ed49624926842a895b57347381
Georgy Trush <48649707+geotrush@users.noreply.github.com>
2021-01-04 21:49:23 +0300

    Update U-Net.py

--- PyTorch/Computer Vision/Semantic Segmentation/U-Net.py
+++ PyTorch/Computer Vision/Semantic Segmentation/U-Net.py
@@ -2,7 +2,7 @@
 "U-Net: Convolutional Networks for Biomedical Image Segmentation" (Ronneberger et al., 2015):
 https://arxiv.org/pdf/1505.04597.pdf
 """
-from torch.nn import Module, Sequential, Conv2d, ConvTranspose2d, ReLU, MaxPool2d, Softmax
+from torch.nn import Module, Sequential, ReflectionPad2d, Conv2d, ConvTranspose2d, ReLU, MaxPool2d, Softmax
 from torch.nn.init import kaiming_normal_, constant_
 
 
@@ -11,42 +11,42 @@ class UNet(Module):
         super().__init__()
 
         # Encoder
-        self.enc1  = Sequential(Conv2d(in_channels, 64, kernel_size=3), ReLU(),
-                                Conv2d(64, 64, kernel_size=3), ReLU())
+        self.enc1  = Sequential(ReflectionPad2d(1), Conv2d(in_channels, 64, kernel_size=3), ReLU(),
+                                ReflectionPad2d(1), Conv2d(64, 64, kernel_size=3), ReLU())
         self.pool1 = MaxPool2d(kernel_size=2, stride=2)
         
-        self.enc2  = Sequential(Conv2d(64, 128, kernel_size=3), ReLU(),
-                               Conv2d(128, 128, kernel_size=3), ReLU())
+        self.enc2  = Sequential(ReflectionPad2d(1), Conv2d(64, 128, kernel_size=3), ReLU(),
+                               ReflectionPad2d(1), Conv2d(128, 128, kernel_size=3), ReLU())
         self.pool2 = MaxPool2d(kernel_size=2, stride=2)
         
-        self.enc3  = Sequential(Conv2d(128, 256, kernel_size=3), ReLU(),
-                                Conv2d(256, 256, kernel_size=3), ReLU())
+        self.enc3  = Sequential(ReflectionPad2d(1), Conv2d(128, 256, kernel_size=3), ReLU(),
+                                ReflectionPad2d(1), Conv2d(256, 256, kernel_size=3), ReLU())
         self.pool3 = MaxPool2d(kernel_size=2, stride=2)
         
-        self.enc4  = Sequential(Conv2d(256, 512, kernel_size=3), ReLU(),
-                                Conv2d(512, 512, kernel_size=3), ReLU())
+        self.enc4  = Sequential(ReflectionPad2d(1), Conv2d(256, 512, kernel_size=3), ReLU(),
+                                ReflectionPad2d(1), Conv2d(512, 512, kernel_size=3), ReLU())
         self.pool4 = MaxPool2d(kernel_size=2, stride=2)
         
         # Bottleneck
-        self.b = Sequential(Conv2d(512, 1024, kernel_size=3), ReLU(),
-                            Conv2d(1024, 1024, kernel_size=3), ReLU())
+        self.b = Sequential(ReflectionPad2d(1), Conv2d(512, 1024, kernel_size=3), ReLU(),
+                            ReflectionPad2d(1), Conv2d(1024, 1024, kernel_size=3), ReLU())
 
         # Decoder
         self.upconv1 = ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
-        self.dec1    = Sequential(Conv2d(1024, 512, kernel_size=3), ReLU(),
-                                  Conv2d(512, 512, kernel_size=3), ReLU())
+        self.dec1    = Sequential(ReflectionPad2d(1), Conv2d(1024, 512, kernel_size=3), ReLU(),
+                                  ReflectionPad2d(1), Conv2d(512, 512, kernel_size=3), ReLU())
         
         self.upconv2 = ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-        self.dec2    = Sequential(Conv2d(512, 256, kernel_size=3), ReLU(),
-                                  Conv2d(256, 256, kernel_size=3), ReLU())
+        self.dec2    = Sequential(ReflectionPad2d(1), Conv2d(512, 256, kernel_size=3), ReLU(),
+                                  ReflectionPad2d(1), Conv2d(256, 256, kernel_size=3), ReLU())
         
         self.upconv3 = ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-        self.dec3    = Sequential(Conv2d(256, 128, kernel_size=3), ReLU(),
-                                  Conv2d(128, 128, kernel_size=3), ReLU())
+        self.dec3    = Sequential(ReflectionPad2d(1), Conv2d(256, 128, kernel_size=3), ReLU(),
+                                  ReflectionPad2d(1), Conv2d(128, 128, kernel_size=3), ReLU())
         
         self.upconv4 = ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.dec4    = Sequential(Conv2d(128, 64, kernel_size=3), ReLU(),
-                                  Conv2d(64, 64, kernel_size=3), ReLU(),
+        self.dec4    = Sequential(ReflectionPad2d(1), Conv2d(128, 64, kernel_size=3), ReLU(),
+                                  ReflectionPad2d(1), Conv2d(64, 64, kernel_size=3), ReLU(),
                                   Conv2d(64, out_channels, kernel_size=1), Softmax())
         
         # Weight Initialization
